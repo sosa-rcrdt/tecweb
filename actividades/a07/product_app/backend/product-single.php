d1ca336a63a520c1b69bd80ed46e7a309537da1c
Nelson <sosafco3@gmail.com>
2024-11-07 23:01:31 -0600

    Actividad 7 - Final

--- actividades/a07/product_app/backend/product-single.php
+++ actividades/a07/product_app/backend/product-single.php
@@ -1,30 +1,4 @@
 <?php
-    /* $data = array();
-
-    if( isset($_POST['id']) ) {
-        $id = $_POST['id'];
-
-        $sql = "SELECT * FROM productos WHERE id = '{$id}'";
-
-        if ( $result = $conexion->query($sql) ) {
-            $rows = $result->fetch_all(MYSQLI_ASSOC);
-
-            if(!is_null($rows)) {
-                foreach($rows as $num => $row) {
-                    foreach($row as $key => $value) {
-                        $data[$num][$key] = utf8_encode($value);  // CODIFICA CADA CAMPO EN UTF-8
-                    }
-                }
-            }
-            $result->free();
-        } else {
-            die('Query Error: '.mysqli_error($conexion));
-        }
-        $conexion->close();
-    }
-
-    echo json_encode($data, JSON_PRETTY_PRINT); */
-
     use TECWEB\MYAPI\Products as Products;
     include_once __DIR__.'/myapi/Products.php';
     $prod = new Products ('marketzone');
